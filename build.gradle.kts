d1d28319a3d8e070b048bd3282505adc3255e695
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-04-20 13:47:54 +0200

    added right JUnit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     scala
-    id("com.github.maiflai.scalatest") version "0.33"
+//    id("com.github.maiflai.scalatest") version "0.33"
 }
 
 tasks.withType<ScalaCompile>().configureEach {
@@ -21,8 +21,10 @@ dependencies {
     // https://mvnrepository.com/artifact/org.scala-lang/scala3-library
     implementation("org.scala-lang:scala3-library_3:${scalaVersion}")
     testImplementation("org.scalatest:scalatest_$scalaMinor:3.2.12")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.64.8")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
