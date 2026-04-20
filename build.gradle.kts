b9626c47147921bc881f066f0b75f8e9d2c95084
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-04-20 11:06:50 +0200

    created ci.yml, created Teacher class and modified build gradle
    # Conflicts:
    #	.github/workflows/ci.yml
    #	build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 plugins {
     java
     scala
     id("com.github.maiflai.scalatest") version "0.33"
 }
+=======
+plugins { java }
+>>>>>>> 0c73a1c (created ci.yml, created Teacher class and modified build gradle)
